cef71501087585937211e96df452694f9ac7e6f2
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-08-21 14:46:26 +0530

    Created basic mathematical functions

---
+++ script.js
@@ -0,0 +1,25 @@
+let num1 = 0;
+let num2 = 0;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(4,2));
+console.log(subtract(4,2));
+console.log(multiply(4,2));
+console.log(divide(4,2));
+
