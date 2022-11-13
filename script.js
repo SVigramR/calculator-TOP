be4f9ee5c4055f74854346292eb179749b882304
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-11-14 02:49:00 +0530

    Calculator Tier-1 functionality
    Dot, power buttons will not work.
    Single operator limit is not done.
    and doesn't worked on error outputs.
    prototype-1 of basic math functions

--- script.js
+++ script.js
@@ -2,37 +2,53 @@ let num1 = '';
 let num2 = '';
 let operator = '';
 let display = '';
-let operation = [];
 
-const oneBtn = document.getElementById("one");
-const twoBtn = document.getElementById("two");
-const threeBtn = document.getElementById("three");
-const fourBtn = document.getElementById("four");
-const fiveBtn = document.getElementById("five");
-const sixBtn = document.getElementById("six");
-const sevenBtn = document.getElementById("seven");
-const eightBtn = document.getElementById("eight");
-const nineBtn = document.getElementById("nine");
-const zeroBtn = document.getElementById("zero");
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
 const equalBtn = document.getElementById("equal");
 const acBtn = document.getElementById("all-clear");
 const clearBtn = document.getElementById("clear");
+const screen = document.getElementById('display');
 
 function clear() {
-    display.textContent = display.textContent
+    screen.textContent = screen.textContent
     .toString()
-    .slice(0, -1)
+    .slice(0, -1);
+    display = display.slice(0, -1);
 }
 
-function operatorOff() {
-    let aButton = document.getElementById('add');
-    let sButton = document.getElementById('subtract');
-    let mButton = document.getElementById('multiply');
-    let dButton = document.getElementById('divide');
-    aButton.disabled = true;
-    sButton.disabled = true;
-    mButton.disabled = true;
-    dButton.disabled = true;
+function allClear() {
+    screen.textContent = '';
+    display = '';
+}
+
+function addNumber(num) {
+    display += num; 
+    show();
+}
+
+function setOperator(operate) {
+    if (display !== '') {
+        display += operate;
+        show();
+    }
+}
+
+function resetScreen() {
+    screen.textContent = '';
+}
+
+function operation() {
+    let inputSplit = display.substring(display.indexOf(0)).split(' ');
+    num1 = inputSplit[0];
+    num2 = inputSplit[2];
+    operator = inputSplit[1];
+    display = operate(operator,num1,num2);
+    show();
+}
+
+function show() {
+    screen.textContent = display;
 }
 
 function add(a, b) {
@@ -51,6 +67,10 @@ function divide(a, b) {
     return a / b;
 }
 
+function power(a, b) {
+    return a ** b;
+}
+
 function operate(operator,a,b) {
     a = Number(a)
     b = Number(b)
@@ -64,15 +84,22 @@ function operate(operator,a,b) {
         case '÷':
             if (a === 0 || b === 0) return null;
             else return divide(a,b);
+        case '^':
+            return power(a,b);
         default:
             return null;
     }
 }
-console.log(add(4,2));
-console.log(subtract(4,2));
-console.log(multiply(4,2));
-console.log(divide(4,2));
 
-window.onload = () => {
-    operatorOff();
-}
+// Event Listeners
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => addNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperator(button.textContent))
+)
+
+equalBtn.onclick = () => operation();
+acBtn.onclick = () => allClear();
+clearBtn.onclick = () => clear();
