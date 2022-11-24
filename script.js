d348029ed36b20b7d3fdc4ea470ccbf04754bcd9
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-11-24 09:00:29 +0530

    Tier-2 calculator completed
    Still power button is pending.
    Few bug fixes to be completed

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
+let battery = true;
 let num1 = '';
 let num2 = '';
 let operator = '';
 let display = '';
 
-const numberButtons = document.querySelectorAll('[data-number]')
-const operatorButtons = document.querySelectorAll('[data-operator]')
+// const powerBtn = document.getElementById('on-off')
+// let allBtn = document.querySelectorAll('.btn').disabled = true;
+const numberBtns = document.querySelectorAll('[data-number]')
+const operatorBtns = document.querySelectorAll('[data-operator]')
 const equalBtn = document.getElementById("equal");
+const dotBtn = document.getElementById("dot");
 const acBtn = document.getElementById("all-clear");
 const clearBtn = document.getElementById("clear");
 const screen = document.getElementById('display');
 
 function clear() {
-    screen.textContent = screen.textContent
-    .toString()
-    .slice(0, -1);
-    display = display.slice(0, -1);
+    if (display !== '') display = display.slice(0, -1); 
+    if (display.endsWith(operator) && operator !== '') {
+        display = display.slice(0, -2);
+        operator = ''; 
+    }
+    splitNumber();
+    show()
 }
 
 function allClear() {
-    screen.textContent = '';
-    display = '';
+    screen.textContent = '', display = '', operator = '', num1 = '', num2 = '';
+}
+
+function splitNumber() {
+    let inputSplit = display.substring(display.indexOf(0)).split(' ');
+    num1 = inputSplit[0], num2 = inputSplit[2];
 }
 
 function addNumber(num) {
     display += num; 
+    splitNumber();
     show();
 }
 
 function setOperator(operate) {
-    if (display !== '') {
+    // const symbols = ['+', '-', '*', '÷', '^']
+    if (display !== '' && !display.toString().includes(operate) && operator === '') {
         display += operate;
-        show();
-    }
+        operator += operate.trim();
+    } 
+    show();
 }
 
-function resetScreen() {
-    screen.textContent = '';
+function addPoint() {
+    if (display === '' || display.endsWith(' ')) display += '0';
+    if (display !== '' && !num1.includes('.') || !num2.includes('.')) display+='.';
+    splitNumber();
+    show();
 }
 
 function operation() {
-    let inputSplit = display.substring(display.indexOf(0)).split(' ');
-    num1 = inputSplit[0];
-    num2 = inputSplit[2];
-    operator = inputSplit[1];
     display = operate(operator,num1,num2);
-    show();
+    num1 = String(display);
+    operator = '', num2 = '';
+}
+
+function equal() {
+    if (num1 !== '' && num2 === '' && operator !== '') display = display;
+    if (num1 !== '' && num2 !== '' && operator !== '') operation();
+    show()
 }
 
 function show() {
@@ -82,7 +102,7 @@ function operate(operator,a,b) {
         case '*':
             return multiply(a,b); 
         case '÷':
-            if (a === 0 || b === 0) return null;
+            if (a === 0 || b === 0) return display = "Can't divide by 0";
             else return divide(a,b);
         case '^':
             return power(a,b);
@@ -92,14 +112,15 @@ function operate(operator,a,b) {
 }
 
 // Event Listeners
-numberButtons.forEach((button) =>
+numberBtns.forEach((button) =>
   button.addEventListener('click', () => addNumber(button.textContent))
 )
 
-operatorButtons.forEach((button) =>
+operatorBtns.forEach((button) =>
   button.addEventListener('click', () => setOperator(button.textContent))
 )
 
-equalBtn.onclick = () => operation();
+equalBtn.onclick = () => equal();
+dotBtn.onclick = () => addPoint();
 acBtn.onclick = () => allClear();
 clearBtn.onclick = () => clear();
